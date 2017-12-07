845409cc8f22692963d2391a189c501fd5b7c0a4
g-amador <g.n.p.amador@gmail.com>
2017-12-07 02:12:51 +0000

    "Updated templates.

---
+++ templates/Core-Toolkit-Components-Project/assets/scripts/GameConstants.js
@@ -0,0 +1,24 @@
+function GameConstant(varName, varValue) {
+    print(varName + " = " + varValue);
+}
+
+//Input devices options
+new GameConstant("useMouse", true);
+new GameConstant("useMouseDebug", false);
+new GameConstant("useMouseCursor", false);
+new GameConstant("useMouseOffScreenPositionShift", true);
+new GameConstant("useKeyBoard", true);
+new GameConstant("useKeyBoardDebug", false);
+
+//Sound options
+new GameConstant("useSoundDebug", false);
+
+//Geometry options
+new GameConstant("showTextures", true);
+new GameConstant("showWireframe", false);
+new GameConstant("useDisplayLists", false);
+
+//GUI options
+new GameConstant("showFPS", true);
+new GameConstant("showText", true);
+new GameConstant("useFullScreen", false);
